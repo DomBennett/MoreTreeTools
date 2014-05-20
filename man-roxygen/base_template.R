5442f6a7416b90ba16211bda1003c0fa30e86578
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-05-20 16:19:00 +0100

    Replaced test_template

---
+++ man-roxygen/base_template.R
@@ -0,0 +1,4 @@
+#' @param tree phylogenetic tree (ape class)
+#' @keywords phylogeny manipulation evolution ecology palaeontology
+#' @references No references yet!
+#' \url{https://github.com/DomBennett/MoreTreeTools}.
--- man-roxygen/test_template.R
+++
@@ -1,4 +0,0 @@
-#' @param phylo phylogenetic tree (ape class)
-#' @keywords phylogeny
-#' @references Me, I (2014) My wonderful paper, All about me, 1:1:1
-#' \url{test.url}.
