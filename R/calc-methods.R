f5608c4537030337cbeb59ad13d134c39ba0bdac
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-05-21 18:43:39 +0100

    Added function to reduceTree

---
+++ R/calc-methods.R
@@ -0,0 +1,71 @@
+#' @name reduceTree
+#' @title Reduce tree by taxonomic rank
+#' @description Reduce tips by identifying tips in the same taxonomic
+#' group at a specifed taxonomic rank
+#' @details Search Global Names Resolver to find shared taxonomic groups of
+#' all tips. Reduce tree by condensing members of the same taxonomic group
+#' into a single tip. The user can specify at what rank the tree should be reduced.
+#' N.B. unresolved names are dropped from the tree and are not represented in tip counts.
+#' @template base_template
+#' @param level rank by which tips will be reduce (e.g. kingdom)
+#' @param datasource a number indicating the GNR datasource to search against
+#' @export
+#' @examples
+#' # example.var <- exampleFun (test.data)
+
+reduceTree <- function (tree, level, datasource = 4) {
+  .match <- function (level, rank) {
+    # a series of patterns to find matching level
+    #  to control for subs, supers etc...
+    matched <- grep (level, rank)
+    pattern <- paste0 ('(super|sub|parv|infra)', level)
+    avoid <- grep (pattern, rank)
+    matched <- matched[!matched %in% avoid]
+    if (length (matched) > 1) {
+      # if still greater than 1, choose at random
+      cat ('Random! In .match\n')
+      matched <- sample (matched, 1)
+    }
+    matched
+  }
+  .pull <- function (lineage, rank) {
+    # extract matching level name
+    lineage <- strsplit (as.character (lineage), split = '\\|')[[1]]
+    rank <- strsplit (as.character (rank), split = '\\|')[[1]]
+    resolved.name <- lineage[.match (level, rank)]
+    if (length (resolved.name) > 0) {
+      return (resolved.name)
+    } else {
+      NA
+    }
+  }
+  # resolve names
+  names <- gsub ("_", " ", tree$tip.label)
+  gnr.obj <- .taxaResolve (names, datasource = datasource)
+  data <- data.frame (lineage = gnr.obj['lineage'], rank = gnr.obj['rank'])
+  # match level to rank, and extract lineage
+  higher.names <- mdply (.data = data, .fun = .pull)[ ,3]
+  # drop all unresolved tips
+  if (any (is.na (higher.names))) {
+    cat (paste0 ("[", sum (is.na (higher.names)),
+                 "] unresovled names have been dropped.\n"))
+    tree <- drop.tip (tree, tree$tip.label[is.na (higher.names)])
+    higher.names <- higher.names[!is.na (higher.names)]
+    if (length (higher.names) < 2) {
+      stop ('Too few tips in tree -- try a different level.')
+    }
+  }
+  # record new names in names.df
+  higher.names.table <- table (higher.names)
+  tip.labels <- paste0 (names (higher.names.table), " -- ", higher.names.table,
+                        " tip(s)")
+  tip.labels <- tip.labels[match (higher.names, names (higher.names.table))]
+  name.df <- data.frame (names = tree$tip.label, higher.names, tip.labels)
+  # drop all non-unique tips
+  to.drop <- -1 * match (unique (tip.labels), tip.labels)
+  tree <- drop.tip (tree, tree$tip.label[to.drop])
+  # rename based on names.df
+  bool <- name.df[['names']] %in% tree$tip.label
+  tree$tip.label <- as.character(name.df[['tip.labels']][bool])
+  tree
+}
