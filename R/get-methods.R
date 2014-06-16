42cd48025192e7e384c18b214575f52eb8ebbdde
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-06-16 15:13:56 +0100

    Added 'all' arg to getAge

--- R/get-methods.R
+++ R/get-methods.R
@@ -41,35 +41,44 @@ getChildren <- function(tree, node, display = FALSE) {
 #' @title Return age of node
 #' @description Return age of tip or internal node
 #'  (so long as the tree provided is time calibrated with branch lengths).
-#' @details No details
+#' @details First calculates the root to tip distance, then calculates
+#' node age by subtracting this distance from the root to node distance.
+#' If \code{node} equals 'all', will return a dataframe for all nodes.
 #' @template base_template
-#' @param node number indicating node
+#' @param node number indicating node (default 'all')
 #' @export
 #' @examples
 #' # example.var <- exampleFun (test.data)
 
-getAge <- function (tree, node) {
-  term.node <- length (tree$tip.label) + 1
-  # if running for multiple nodes, define tree.age first to
-  #  save time
-  if (!exists ('tree.age')) {
-    tree.age <- max (diag (vcv.phylo (tree)))
+getAge <- function (tree, node = 'all') {
+  run <- function (node) {
+    # function for calculating age of node
+    term.node <- length (tree$tip.label) + 1
+    # if it's the root node, return tree.age
+    if (term.node == node) {
+      return (tree.age)
+    }
+    # if it's a tip return 0
+    if (node < length (tree$tip.label) & is.ultrametric (tree)) {
+      return (0)
+    }
+    # else find all its edge.lengths and subtract from tree.age
+    edges <- c ()
+    while (node != term.node) {
+      edges <- c (edges, which (tree$edge[ ,2] == node))
+      node <- tree$edge[tree$edge[ ,2] == node, 1]
+    }
+    return (tree.age - sum (tree$edge.length[edges]))
   }
-  # if it's the root node, return tree.age
-  if (term.node == node) {
-    return (tree.age)
+  tree.age <- max (diag (vcv.phylo (tree)))
+  if (node != 'all') {
+    return (run (node))
   }
-  # if it's a tip return 0
-  if (node < length (tree$tip.label) & is.ultrametric (tree)) {
-    return (0)
-  }
-  # else find all its edge.lengths and subtract from tree.age
-  edges <- c ()
-  while (node != term.node) {
-    edges <- c (edges, which (tree$edge[ ,2] == node))
-    node <- tree$edge[tree$edge[ ,2] == node, 1]
-  }
-  return (tree.age - sum (tree$edge.length[edges]))
+  # else node == all, run on all nodes
+  nodes <- 1:(length (tree$tip.label) + tree$Nnode)
+  res <- mdply (.data = data.frame (node = nodes), .fun = run)
+  colnames (res)[2] <- 'age'
+  res
 }
 
 #' @name getSister
