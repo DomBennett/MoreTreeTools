0ec804dd2f4e4796bbd2efc00790c12e55624896
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-10-26 17:52:02 +0000

    Moved chromatophylo to viz, started testing

--- R/viz-methods.R
+++ R/viz-methods.R
@@ -59,3 +59,257 @@ commplot <- function(cmatrix, tree, groups = rep(1, nrow(cmatrix)),
     group <- groups[i]
   }
 }
+
+#' @name chromatophylo
+#' @title Plot tree through time with coloured edges
+#' @description Return a geom_object() of a phylogenetic tree through time.
+#' Requires tree given to be rooted, time callibrated and bifurcating.
+#' @details No details
+#' @param tree
+#' @export
+#' @examples
+#' # Quick example without colours
+#' tree <- rtree (50)  # random tree with fossils
+#' plot (tree)  # ape plot
+#' chromatophylo (tree)  # chromatophylo
+#' 
+#' 
+#' # Example with collapseTips() + calcEdgeDiversity()
+#' # generate random tree
+#' tree <- rtree (100)
+#' # add edge labels to track edges after collapse
+#' tree$edge.label <- paste0 ('edge_', 1:nrow (tree$edge))
+#' # generate edge diversity using two intervals
+#' ed <- calcEdgeDiversity (tree, n.intervals=2)
+#' # iteratively drop all tip edges with > 10% tree age
+#' tree.age <- getSize (tree, 'rtt')
+#' ctree <- collapseTips (tree, min.length=tree.age*0.1, iterative=TRUE)
+#' # reconstruct edge diversity data frame without dropped edges
+#' new.ed <- data.frame (edge.label=ctree$edge.label)
+#' new.ed$edge <- 1:nrow (new.ed)
+#' match.counts <- match (ctree$edge.label, ed$edge.label)
+#' new.ed$count <- ed$count[match.counts]
+#' # convert count to logged Z-score for maximum colour separation
+#' new.ed$col <- (log (new.ed$count) - mean (log (new.ed$count))) /
+#'   sd (log (new.ed$count))
+#' ed$col <- (log (ed$count) - mean (log (ed$count))) /
+#'   sd (log (ed$count))
+#' # plot before
+#' chromatophylo (tree, edge.cols=ed, legend.title='Diversity') + ggtitle ('Before collapse')
+#' # plot after
+#' chromatophylo (ctree, edge.cols=new.ed, legend.title='Diversity') + ggtitle ('After collapse')
+
+# TODO:
+# 1. Take tree data from Newick file efficiently
+# 2. reverse x axis
+# 3. add tips option
+# 4. test
+# 5. speed up
+
+chromatophylo <- function (tree, edge.cols=NULL, edge.sizes=NULL, legend.title='') {
+  if (!is.binary.tree (tree)) {
+    stop ('Tree must be bifurcating')
+  }
+  # init plot data
+  N <- length (tree$tip.label)
+  tree.age <- getSize (tree, 'rtt')
+  x.length <- tree.age/100
+  y.length <- 1
+  x1 <- 0
+  node <- length (tree$tip.label) + 1
+  edge <- which (tree$edge[ ,1] == node)
+  # init p.env
+  p.env <- new.env (parent=emptyenv ())
+  p.env$p.data <- data.frame (x=c(0, 0), y=c(0, y.length),
+                              edge=c (0,0))
+  p.env$y.length <- y.length
+  p.env$x.length <- x.length
+  p.env$tree <- tree
+  p.env$root.node <- length (tree$tip.label) + 1
+  #t.data <- data.frame (x=NA, y=NA, label=NA)
+  # generate p.data
+  .cpMkLine (x1, c (0, y.length), edge, p.env)
+  while (TRUE) {
+    # loop through p.data rearranging y coords to avoid overlap
+    p.env$overlap <- FALSE
+    lines <- seq (from=3, to=nrow (p.env$p.data), by=2)
+    m_ply (.data=data.frame (sl=lines), .fun=.cpEachSbjct, lines=lines,
+           p.env=p.env)
+    #return (p.env$p.data)
+    print ('here')
+    if (!p.env$overlap) {
+      break
+    }
+  }
+  # make geom_object
+  p.data <- p.env$p.data
+  if (!is.null (edge.cols)) {
+    matched.is <- match (p.data$edge, edge.cols$edge)
+    p.data$col <- edge.cols$col[matched.is]
+  }
+  if (!is.null (edge.sizes)) {
+    matched.is <- match (p.data$edge, edge.sizes$edge)
+    p.data$size <- edge.sizes$col[matched.is]
+  }
+  p <- ggplot (p.data, aes (x=x, y=y, group=edge))
+  colscale <- scale_colour_gradient2 (low="blue", mid='white',
+                                      high='red', name=legend.title)
+  if (all (c ('col', 'size') %in% names (p.data))) {
+    p <- p + geom_line (aes (colour=col, size=size)) + colscale
+  } else if ('size' %in% names (p.data)) {
+    p <- p + geom_line (aes (size=size), colour='white')
+  } else if ('col' %in% names (p.data)) {
+    p <- p + geom_line (aes (colour=col), size=2)  + colscale
+  } else {
+    p <- p + geom_line (colour='white', size=2)
+  }
+  # set ggplot environment parameters
+  theme.opts <- theme(panel.background = element_rect(fill="gray15"),
+                      axis.title.y = element_blank(),
+                      axis.ticks.y =element_blank(),
+                      axis.text.y = element_blank(),
+                      panel.grid.major.y = element_blank(),
+                      panel.grid.minor.y = element_blank(),
+                      panel.grid.major.x = element_line(colour="black"),
+                      panel.grid.minor.x = element_line(colour="gray5"))
+  
+  p <- p + xlab ('Time') + theme.opts
+  p
+}
+.cpEachQry <- function (ql, p.env, avoid.edges, sbjct, sl.edge) {
+  # unpack p.env
+  p.data <- p.env$p.data
+  y.length <- p.env$y.length
+  x.length <- p.env$x.length
+  # get qry
+  qry <- p.data[ql:(ql+1), ]
+  ql.edge <- unique (qry$edge)
+  if (!ql.edge %in% avoid.edges) {
+    overlap <- .cpCheckOverlap (sbjct, qry, y.length, x.length)
+    if (overlap) {
+      # set overlap to TRUE
+      p.env$overlap <- TRUE
+      # find all edges descdning from higher edge and add y.length/2
+      # find all edges descending from lower edge and remove y.length/2
+      # find parent vertical edge and add and remove y.length/2
+      parent.node <- getParent (p.env$tree, edges=c (sl.edge, ql.edge))
+      if (parent.node == p.env$root.node) {
+        parent.edge <- 0
+      } else {
+        parent.edge <- which (p.env$tree$edge[ ,2] == parent.node)
+      }
+      edges <- which (p.env$tree$edge[ ,1] == parent.node)
+      edge.1 <- p.data[p.data$edge == edges[1], ]
+      edge.2 <- p.data[p.data$edge == edges[2], ]
+      if (mean (edge.1$y) >= mean (edge.2$y)) {
+        higher <- edge.1$edge[1]
+        lower <- edge.2$edge[1]
+      } else {
+        higher <- edge.2$edge[1]
+        lower <- edge.1$edge[1]
+      }
+      higher.edges <- c (getEdges (p.env$tree, node=p.env$tree$edge[higher, 2]),
+                         higher)
+      p.data[p.data$edge %in% higher.edges, 'y'] <-
+        p.data[p.data$edge %in% higher.edges, 'y'] + y.length/2
+      lower.edges <- c (getEdges (p.env$tree, node=p.env$tree$edge[lower, 2]),
+                        lower)
+      p.data[p.data$edge %in% lower.edges, 'y'] <-
+        p.data[p.data$edge %in% lower.edges, 'y'] - y.length/2
+      vertical.bool <- which (p.data$edge == parent.edge)
+      vertical.bool <- vertical.bool[(length (vertical.bool) - 1):
+                                       length (vertical.bool)]
+      p.data[vertical.bool[2], 'y'] <- p.data[vertical.bool[2], 'y'] + y.length/2
+      p.data[vertical.bool[1], 'y'] <- p.data[vertical.bool[1], 'y'] - y.length/2
+      # update p.data in p.env
+      p.env$p.data <- p.data
+    }
+  }
+}
+.cpCheckOverlap <- function (sbjct, qry, y.length, x.length) {
+  # check if sbjct and qry straight lines overlap (within y.length)
+  maxqx <- max (qry$x) + x.length/4
+  minqx <- min (qry$x) - x.length/4
+  minqy <- min (qry$y) - y.length/4
+  maxqy <- max (qry$y) + y.length/4
+  maxsx <- max (sbjct$x) + x.length/4
+  minsx <- min (sbjct$x) - x.length/4
+  minsy <- min (sbjct$y) - y.length/4
+  maxsy <- max (sbjct$y) + y.length/4
+  xspace <- maxsx > minqx & minsx < maxqx
+  yspace <- maxsy > minqy & minsy < maxqy
+  ycross <- maxsy > maxqy & minsy < minqy
+  xcross <- maxsx > maxqx & minsx < minqx
+  xspace & ycross | yspace & xcross |
+    yspace & xspace
+}
+.cpEachSbjct <- function (sl, lines, p.env) {
+  sbjct <- p.env$p.data[sl:(sl+1), ]
+  # find connecting edges
+  sl.edge <- unique (sbjct$edge)
+  nodes <- p.env$tree$edge[sl.edge, ]
+  avoid.edges <- c (which (p.env$tree$edge[ ,2] == nodes[1]),
+                    which (p.env$tree$edge[ ,1] == nodes[2]),
+                    sl.edge)
+  m_ply (.data=data.frame (ql=lines), .cpEachQry, p.env=p.env,
+         avoid.edges=avoid.edges, sbjct=sbjct, sl.edge=sl.edge)
+}
+.cpMkLine <- function (x1, y, edge, p.env) {
+  if (length (edge) == 2) {
+    # run recursively for both
+    .cpMkLine (x1, y[1], edge[1], p.env)
+    .cpMkLine (x1, y[2], edge[2], p.env)
+  } else if (length (edge) == 1){
+    # find the next node on the edge
+    next.node <- p.env$tree$edge[edge,2]
+    # get n.children
+    n.children <- length (getChildren (p.env$tree, next.node))
+    # get the length of the edge
+    x2 <- x1 + p.env$tree$edge.length[edge]
+    l.data <- data.frame (x=c(x1, x2), y=c(y, y), edge)
+    p.env$p.data <- rbind (p.env$p.data, l.data)
+    # run again for next edge
+    next.edge <- which (p.env$tree$edge[ ,1] == next.node)
+    if (next.node <= length (p.env$tree$tip.label)) {
+      # EDIT THIS TO ADD TIP LABELS
+      #t.data <<- data.frame (x=(x2+1/N), y=y,
+      #                       label=tree$tip.label[next.node])
+    } else {
+      ys <- c (y-(p.env$y.length/2), y+(p.env$y.length/2))
+      # add vertical connector
+      l.data <- data.frame (x=c(x2, x2), y=ys, edge)
+      p.env$p.data <- rbind (p.env$p.data, l.data)
+      # move on to next edge
+      .cpMkLine (x2, ys, next.edge, p.env)
+    }
+  } else {
+    stop ('Invalid tree')
+  }
+}
+
+.cpGetNChildren <- function (p.env) {
+  # generate list of n.children
+  .count <- function (node) {
+    n.children <- length (getChildren (p.env$tree, node=node))
+    res[[node]] <<- n.children
+  }
+  res <- list ()
+  nnodes <- length (p.env$tree$tip.label) +
+    p.env$tree$Nnode
+  l.data <- data.frame (node=1:nnodes, stringsAsFactors=FALSE)
+  m_ply (.data=l.data, .fun=.count)
+  p.env$n.children <- res
+}
+
+.cpGetParentNode <- function (p.env) {
+  # create list where when two given are given, parent node is returned
+}
+
+.cpGetDescendingEdges <- function (p.env) {
+  # create list where edges are returned for node given
+}
+
+# Generate before hand:
+# 1. number of children per node -- done
+# 2. parent node of two edges
+# 3. all descending edges by node
--- tests/testthat/test-viz-methods.R
+++ tests/testthat/test-viz-methods.R
@@ -1,1 +1,112 @@
-# TODO
+# Test viz-methods
+# D.J. Bennett
+# 26/10/2015
+
+# LIBS
+library (MoreTreeTools)
+library (testthat)
+
+# FUNCTIONS
+init <- function () {
+  # push random tree and key parameters
+  tree <<- rtree (10)
+  N <<- length (tree$tip.label)
+  tree.age <- getSize (tree, 'rtt')
+  x.length <<- tree.age/100
+  y.length <<- 1
+  x1 <<- 0
+  node <<- length (tree$tip.label) + 1
+  edge <<- which (tree$edge[ ,1] == node)
+  # init p.env
+  p.env <<- new.env (parent=emptyenv ())
+  p.env$p.data <- data.frame (x=c(0, 0), y=c(0, y.length),
+                              edge=c (0,0))
+  p.env$y.length <- y.length
+  p.env$x.length <- x.length
+  p.env$tree <- tree
+  p.env$root.node <- length (tree$tip.label) + 1
+}
+
+
+# RUNNING
+context ('Testing \'viz-methods\'')
+test_that ('.cpMkLine([basic]) works', {
+  # init
+  init ()
+  # run
+  .cpMkLine (x1, c (0, y.length), edge, p.env)
+  # sanity check
+  res <- max (p.env$p.data$edge) == nrow (tree$edge)
+  expect_that (res, is_true ())
+  res <- max (p.env$p.data$x) == getSize (tree, 'rtt')
+  expect_that (res, is_true ())
+})
+
+test_that ('.cpCheckOverlap([basic]) works', {
+  # 1 spacer in x and y (lines must be without 0.5 from each other)
+  x.length <- y.length <- 1
+  # parallel lines (overlap is false)
+  sbjct <- data.frame (x = c (0, 1),
+                       y = c (1, 1))
+  qry <- data.frame (x = c (0, 1),
+                     y = c (2, 2))
+  res <- .cpCheckOverlap (sbjct, qry, y.length, x.length)
+  expect_that (res, is_false ())
+  # crossing lines (overlap is true)
+  sbjct <- data.frame (x = c (0, 1),
+                       y = c (1, 1))
+  qry <- data.frame (x = c (0.5, 0.5),
+                     y = c (0.5, 1.5))
+  res <- .cpCheckOverlap (sbjct, qry, y.length, x.length)
+  expect_that (res, is_true ())
+  # parallel lines, too close (overlap is true)
+  sbjct <- data.frame (x = c (0, 1),
+                       y = c (1, 1))
+  qry <- data.frame (x = c (0, 1),
+                     y = c (1.49, 1.49))
+  res <- .cpCheckOverlap (sbjct, qry, y.length, x.length)
+  expect_that (res, is_true ())
+  # parallel lines, at the right distance (overlap is false)
+  sbjct <- data.frame (x = c (0, 1),
+                       y = c (1, 1))
+  qry <- data.frame (x = c (0, 1),
+                     y = c (1.51, 1.51))
+  res <- .cpCheckOverlap (sbjct, qry, y.length, x.length)
+  expect_that (res, is_false ())
+  # successive lines (overlap is true)
+  # (this scenario would usually be ignored as this is expected
+  #  if lines are descending)
+  sbjct <- data.frame (x = c (0, 1),
+                       y = c (1, 1))
+  qry <- data.frame (x = c (1, 2),
+                     y = c (1, 1))
+  res <- .cpCheckOverlap (sbjct, qry, y.length, x.length)
+  expect_that (res, is_true ())
+})
+
+test_that ('.cpEachQry([basic]) works', {
+  # init
+  init ()
+  .cpMkLine (x1, c (0, y.length), edge, p.env)
+  # run 1  -- edge is avoided
+  ql <- 0
+  sl.edge <- 1
+  .cpEachQry (ql, p.env, avoid.edges, sbjct, sl.edge)
+  expect_that (p.env$p.data, equals (p.data))
+  # run 2 -- edge is not avoided
+  avoid.edges <- c (4)
+  .cpEachQry (ql, p.env, avoid.edges, sbjct, sl.edge)
+  expect_that (p.env$p.data, equals (p.data))
+  # run 3  -- edge no longer overlaps
+  .cpEachQry (ql, p.env, avoid.edges, sbjct, sl.edge)
+  expect_that (p.env$p.data, equals (p.data))
+})
+
+test_that ('.cpGetNChildren([basic]) works', {
+  # init
+  init ()
+  # sanity checks
+  .cpGetNChildren (p.env)
+  expect_that (p.env$n.children[[11]], equals (10))
+  expect_that (p.env$n.children[[1]], equals (1))
+})
