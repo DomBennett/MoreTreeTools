e19aecb92d10acad3aa5d7e36bf8a7910525ee50
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-11-09 17:44:50 +0000

    Created replacer methods for NodeList

--- other/development.R
+++ other/development.R
@@ -22,8 +22,8 @@ setClass ('Node', representation=node_representation)
 setGeneric ('print')
 setMethod ('print', c('x'='Node'),
            function(x){
-             cat ('Node Object:\n\tID = [', x@node_id, '], children = [',
-                  length (x@children), '], age = [',
+             cat ('Node: ID=[', x@node_id, '], children=[',
+                  length (x@children), '], age=[',
                   x@max_age, ']', sep='')
            })
 setMethod ('show', 'Node',
@@ -31,15 +31,19 @@ setMethod ('show', 'Node',
              print (object)
            })
 # NodeList declaration
-nodelist_representation <- representation (nodes='list')
+nodelist_representation <- representation (nodes='list',
+                                           elements='vector',
+                                           ntips='numeric')
 prototype_root_node <- new ('Node', node_id=1L)
-nodelist_prototype <- prototype (nodes=list(prototype_root_node))
+nodelist_prototype <- prototype (nodes=list(prototype_root_node),
+                                 elements=c('children', 'age'),
+                                 ntips=1)
 setClass ('NodeList', representation=nodelist_representation,
           prototype=nodelist_prototype)
 setGeneric ('print')
 setMethod ('print', c('x'='NodeList'),
            function(x){
-             cat ('NodeList Object of [',
+             cat ('NodeList: [',
                   length (x@nodes), '] nodes', sep='')
            })
 setMethod ('show', 'NodeList',
@@ -51,37 +55,77 @@ setMethod ('[', c ('NodeList', 'numeric', 'missing', 'ANY'),
              i <- as.integer (i)
              initialize(x, nodes=x@nodes[i])
            })
+setMethod ('[<-', c ('NodeList', 'numeric', 'missing', 'NodeList'),
+           function(x, i, j, ..., value) {
+             i <- as.integer (i)
+             x@nodes[i] <- initialize(value)
+             initialize(x)
+           })
 setMethod ('[[', c ('NodeList', 'numeric', 'missing'),
            function(x, i, j, ...) {
              i <- as.integer (i)
-             x@nodes[[i]]
+             initialize(x@nodes[[i]])
            })
-setGeneric ('addNode', signature= c('x', 'parent_node', 'min_age',
-                                    'max_age', 'tip'),
-            function(x, parent_node, min_age, max_age, tip) {
-              genericFunction ('addNode')
-              })
-setMethod ('addNode', c('x'='NodeList', 'parent_node'='integer',
-                        'min_age'='numeric', 'max_age'='numeric',
-                        'tip'=ANY),
-           function(x, parent_node, min_age, max_age, tip=NA) {
-             node_id <- as.integer (length (x@nodes) + 1)
-             children <- c (x@nodes[[parent_node]]@children, tip)
-             node <- new ('Node', min_age=min_age, max_age=max_age,
-                          prev_node=parent_node, node_id=node_id,
-                          children=children)
-             x@nodes[[node_id]] <- node
-             return (x)
+setMethod ('[[<-', c ('NodeList', 'numeric', 'missing', 'Node'),
+           function(x, i, j, ..., value) {
+             i <- as.integer (i)
+             x@nodes[[i]] <- initialize (value)
+             initialize(x)
            })
+update <- function (nodelist, node_id) {
+  # Recursively update each nodelist element from node_id to root
+  .update <- function (nodelist, node_id, next_node_id) {
+    node <- nodelist[[node_id]]
+    next_node <- nodelist[[next_node_id]]
+    # for (element in nodelist@elements)
+    node@children <- c (node@children, next_node@children)
+    # TODO: fix age update
+    test_age <- (node@age + next_node@age)
+    node@age <- ifelse (test_age > node@age, next_node@age,
+                        test_age)
+    nodelist@nodes[[node_id]] <- node
+    if (length (nodelist[[node_id]]@prev_node) > 0) {
+      nodelist <- .update (nodelist, node@prev_node, node_id)
+    }
+    nodelist
+  }
+  node <- nodelist[[node_id]]
+  if (length (node@prev_node) > 0) {
+    nodelist <- .update (nodelist, node@prev_node, node_id)
+  }
+  nodelist
+}
+
 nodelist <- new('NodeList')
-nodelist <- addNode(nodelist, parent_node=1L, min_age=0, max_age=1, tip=NA)
-nodelist[3:4]
-str(nodelist@nodes[[2]])
+nodelist <- addNode(nodelist, parent_node=1L, min_age=0, max_age=1, tip='t1')
+nodelist <- addNode(nodelist, parent_node=1L, min_age=0, max_age=0.5, tip='t2')
 
-initialize (nodelist, nodes=list (nodelist@nodes[[1]]))
+addNode <- function(nodelist, parent_node, min_age, max_age, tip=NA) {
+  node_id <- as.integer (length (nodelist@nodes) + 1)
+  children <- c (nodelist@nodes[[parent_node]]@children, tip)
+  node <- new ('Node', min_age=min_age, max_age=max_age,
+              prev_node=parent_node, node_id=node_id,
+              children=children)
+  nodelist@nodes[[node_id]] <- node
+  nodelist <- update (nodelist, node_id)
+  return (nodelist)
+}
 
-# Tree declaration
-tree_representation <- representation (nodes='NodeList')
-setClass ('TreeList', representation=tree_representation,
-          prototype=prototype (nodes=new('NodeList')))
-new ('TreeList')
+
+# setGeneric ('addNode', signature= c('x', 'parent_node', 'min_age',
+#                                     'max_age', 'tip'),
+#             function(x, parent_node, min_age, max_age, tip) {
+#               genericFunction ('addNode')
+#             })
+# setMethod ('addNode', c('x'='NodeList', 'parent_node'='integer',
+#                         'min_age'='numeric', 'max_age'='numeric',
+#                         'tip'=ANY),
+#            function(x, parent_node, min_age, max_age, tip=NA) {
+#              node_id <- as.integer (length (x@nodes) + 1)
+#              children <- c (x@nodes[[parent_node]]@children, tip)
+#              node <- new ('Node', min_age=min_age, max_age=max_age,
+#                           prev_node=parent_node, node_id=node_id,
+#                           children=children)
+#              x@nodes[[node_id]] <- node
+#              return (x)
+#            })
