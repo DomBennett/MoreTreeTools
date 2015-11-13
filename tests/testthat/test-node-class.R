8e1e71d39160041ddf23685ec4934cccbf0239e2
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-11-13 18:58:38 +0000

    Init test files for new classes

---
+++ tests/testthat/test-node-class.R
@@ -0,0 +1,13 @@
+# Test Node Class
+# D.J. Bennett
+# 13/11/2015
+
+# LIBS
+library (MoreTreeTools)
+library (testthat)
+
+# TEST DATA
+
+# RUNNING
+context ('Testing \'Node Class\'')
+#TODO
---
+++ tests/testthat/test-nodelist-class.R
@@ -0,0 +1,13 @@
+# Test NodeList Class
+# D.J. Bennett
+# 13/11/2015
+
+# LIBS
+library (MoreTreeTools)
+library (testthat)
+
+# TEST DATA
+
+# RUNNING
+context ('Testing \'NodeList Class\'')
+#TODO
