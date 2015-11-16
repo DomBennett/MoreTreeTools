f381833dcc92d6d02545d0acb2c7e552a074bfc8
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-11-16 15:54:33 +0000

    Init testing of new get-methods

---
+++ tests/testthat/test-get-methods-visible.R
@@ -0,0 +1,91 @@
+# Test get-methods
+# D.J. Bennett
+# 05/05/2014
+
+# LIBS
+library (MoreTreeTools)
+library (testthat)
+
+# TEST DATA
+
+# RUNNING
+context ('Testing \'get-methods\'')
+test_that ('getNodeStats([basic]) works', {
+  
+})
+
+test_that ('getEdgeStats([basic]) works', {
+  
+})
+
+test_that ('getOutgroup([basic]) works',{
+  
+})
+
+test_that ('getExtant([basic]) works',{
+  
+})
+
+test_that ('getChildren([basic]) works',{
+  
+})
+
+test_that ('getSize([basic]) works', {
+  
+})
+
+test_that ('getAge([basic]) works', {
+  
+})
+
+test_that ('getSister([basic]) works', {
+  
+})
+
+test_that ('getParent([node specified]) works', {
+  
+})
+
+test_that ('getParent([tips specified]) works', {
+  
+})
+
+test_that ('getParents([edges specified]) works', {
+  
+})
+
+test_that ('getEdges([node specified]) works', {
+  
+})
+
+test_that ('getEdges([tips specified, type = 1]) works', {
+  
+})
+
+test_that ('getEdges([tips specified, type = 2]) works', {
+  
+})
+
+test_that ('getEdges([tips specified, type = 3]) works', {
+  
+})
+
+test_that ('getNodes([basic]) works',{
+  
+})
+
+test_that ('getClades([basic]) works',{
+  
+})
+
+test_that ('getNodeLabels([basic]) works', {
+  
+})
+
+test_that ('getSubtrees([basic]) works', {
+  
+})
+
+test_that ('getTreeStats([basic]) works', {
+  
+})
