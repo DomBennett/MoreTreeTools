147a4ed9bbc545f88051d09ff2275885dcbca4b1
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-12-01 19:07:59 +0000

    Switched to using predist rather than age
     — predict is easier to calculate

--- other/development.R
+++ other/development.R
@@ -5,6 +5,18 @@ tree <- as (tree_phylo, 'NodeList')
 getSize (tree_phylo, 'rtt')
 sum (tree_phylo$edge.length)
 
-plot (tree_phylo);nodelabels()
+plot (tree_phylo);nodelabels();axisPhylo()
 getAge (tree_phylo, 5)
-tree[['t5']]$age
+age (tree)
+tree[['t2']]$predist
+
+
+# adding new tip
+edge <- 'n7'
+tip_name <- 't6'
+node_name <- 'n10'
+node_age <- 1.8
+tip_age <- -1  # test whether negative numbers increases age of tree
+tree <- MoreTreeTools:::.addTip__NodeList (tree, edge, tip_name, node_name,
+                           node_age, tip_age)
+tree
