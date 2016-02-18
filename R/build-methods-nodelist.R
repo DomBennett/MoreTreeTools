e73e8679abaabf3943070736c064ffa7f747694e
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-02-18 10:06:06 +0000

    Revert "Reworking nodelist"
    This reverts commit 104dce8c4e5171a0c6edbf1d0d8061403e118094.

--- R/build-methods-nodelist.R
+++ R/build-methods-nodelist.R
@@ -1,28 +1,28 @@
 .addTip__NodeList <- function (tree, edge, tip_name, node_name,
                                node_age, tip_age=0) {
-  # edge refers to the sister of the new tip to be added
+  # edge refers to the sister tip
   sister <- tree@nodelist[[edge]]
   # look up prenode and up date
   parent <- sister$prenode[[1]]
-  parent_age <- .getAge__NodeList (tree, parent)
+  parent_age <- tree@age - parent$preDist()
   # create new internal node
-  new_node <- list ('id'=node_name, 'span'=parent_age-node_age,
-                   'prenode'=c (parent))
+  new_node <- new ('Node', id=node_name, span=parent_age-node_age,
+                   prenode=c (parent))
   # create new tip
-  new_tip <- list ('id'=tip_name, 'span'=node_age-tip_age,
-                  'prenode'=c (new_node$id))
-  new_node$postnode <- c (sister$id, new_tip$id)
+  new_tip <- new ('Node', id=tip_name, span=node_age-tip_age,
+                  prenode=c (new_node))
+  new_node$postnode <- c (sister, new_tip)
   # update parent's postnode
-  parent_postnodes <- tree@nodelist[[parent]]$postnode
-  parent_postnodes <- parent_postnodes[parent_postnodes != edge]
-  tree@nodelist[[parent]]$postnode <- c (parent_postnodes, new_node$id)
+  parent_postnodes <- parent$postnode[which (sapply (parent$postnode,
+                                                     function (x) x$id) != edge)]
+  parent_postnodes <- c (parent_postnodes, new_node)
   # update sister's prenode
-  tree@nodelist[[edge]]$prenode <- c (new_node$id)
+  sister$prenode <- c (new_node)
   # add to NodeList
   tree@nodelist[[node_name]] <- new_node
   tree@nodelist[[tip_name]] <- new_tip
   # Update
-  tree
+  .update (tree)
 }
 .removeTip__NodeList <- function (tree, tips, preserve_age) {
   cat ('Not yet implemented for NodeList')
