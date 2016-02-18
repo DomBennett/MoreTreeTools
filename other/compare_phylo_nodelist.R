dc9925fe245cbc4d9800c33527c5b979cb6ae8ff
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-02-18 10:06:07 +0000

    Revert "Demonstrated nodelist is faster than phylo"
    This reverts commit aca6f96662ed7649eeab442b2454b9a444ebb55a.

--- other/compare_phylo_nodelist.R
+++ other/compare_phylo_nodelist.R
@@ -8,9 +8,6 @@
 # 1. Grow tree from 3 tips to 100
 # 2. Time how long it takes for phylo and Nodelist
 
-# LIBS
-library (MoreTreeTools)
-
 # --------------
 # Phylo function
 # --------------
@@ -35,73 +32,53 @@ phyloBuilder <- function (tree, iterations) {
 # NodeList function
 # -----------------
 nodeListBuilder <- function (tree, iterations) {
-  for (i in 4:(iterations+3)) {
-    print (i)
+  for (i in (nNodes (tree)+1):(nTips (tree)+iterations)) {
     # add tip
-    tree <- .addTip__NodeList (tree, edge='t1',
-                               tip_age=0, node_age=0.5,
-                               tip_name=paste0('t', i),
-                               node_name=paste0('n', i+2))
+    tree <- MoreTreeTools:::.addTip__NodeList (tree, edge='t1',
+                                               tip_age=0, node_age=0.5,
+                                               tip_name=paste0('t', i),
+                                               node_name=paste0('n', i+2))
   }
   tree
 }
 
-# ------------------------
-# Pseudo-NodeList function
-# ------------------------
-pseudoNodeListBuilder <- function (iterations) {
-  #setClass ('Node', representation=representation(i='numeric'))
-  tree <- list ()
-  for (i in 1:iterations) {
-    # create a new S4 class object and add to list
-    #node <- new ('Node', i=i)
-    node <- list (i)
-    tree[[i]] <- node
-  }
+# -------
+# Testing
+# -------
+iterations <- 1000
+start_tree <- compute.brlen (stree (3, 'left'))
+start_time <- Sys.time ()
+tree <- phyloBuilder (start_tree, iterations)
+end_time <- Sys.time ()
+phylo_time <- end_time - start_time
+rm (tree)
+# start_tree <- as (start_tree, 'NodeList')
+# start_time <- Sys.time ()
+# tree <- nodeListBuilder (start_tree, iterations)
+# end_time <- Sys.time ()
+# nodelist_time <- end_time - start_time
+start_time <- Sys.time ()
+tree <- list ()
+for (i in 1:iterations) {
+  tree[[i]] <- i
 }
+end_time <- Sys.time ()
+nodelist_time <- end_time - start_time
+cat ('\nphylo time: ', phylo_time)
+cat ('\nPseudo-NodeList time: ', nodelist_time)
 
-
-# ---------
-# Profiling
-# ---------
 # Really slow...
 # Rprof(tmp <- tempfile())
 # tree <- nodeListBuilder (start_tree, iterations)
 # Rprof()
 # summaryRprof(tmp)
 
+# New thinks:
+# The profile suggests that it is the creation of Node objects
+#  that take time. I think I shold convert the Node objects
+#  to S4 class or even just to a simple list. Either way it should be
+#  lightweight. The major issue with the RefClass is it takes a copy
+#  of all the methods associated with the object as well as the data.
 
-# -------
-# Testing
-# -------
-repeats <- 100
-res <- data.frame (iterations=NA, timing=NA, builder=NA)
-start_phylo <- compute.brlen (stree (3, 'left'))
-start_nodelist <- as (start_phylo, 'NodeList')
-for (iterations in (1:repeats*10)) {
-  # phylo
-  start_time <- Sys.time ()
-  tree <- phyloBuilder (start_phylo, iterations)
-  end_time <- Sys.time ()
-  phylo_time <- end_time - start_time
-  rm (tree)
-  # nodelist
-  start_time <- Sys.time ()
-  tree <- nodeListBuilder (start_nodelist, iterations)
-  end_time <- Sys.time ()
-  nodelist_time <- end_time - start_time
-  rm (tree)
-  # pseudo-nodelist
-  start_time <- Sys.time ()
-  pseudoNodeListBuilder (iterations)
-  end_time <- Sys.time ()
-  pnodelist_time <- end_time - start_time
-  #cat ('\nphylo time: ', phylo_time)
-  #cat ('\nNodeList time: ', nodelist_time)
-  #cat ('\nPseudo-NodeList time: ', pnodelist_time)
-  res <- rbind (res, data.frame (iterations=iterations, timing=c (phylo_time, nodelist_time,
-                                                                  pnodelist_time),
-                                 builder=c('phylo', 'nodelist', 'pnodelist')))
-}
-p <- ggplot (res, aes (y=timing, x=iterations, colour=builder)) + geom_line()
-print (p)
+
+
